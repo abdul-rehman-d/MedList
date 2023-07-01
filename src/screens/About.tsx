437e33bd198202d31333283cce067df474422ff8
Abdul Rehman <ar618211@gmail.com>
2023-07-01 17:37:55 +0500

    about done

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,7 +1,8 @@
-import { ScrollView, View } from 'react-native'
+import { Linking, View } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types';
 import CustomAppBar from '../components/ui/CustomAppBar';
+import { Text } from 'react-native-paper';
 
 function About({ route, navigation }: NativeStackScreenProps<RootStackParamList, 'About'>) {
 
@@ -13,11 +14,39 @@ function About({ route, navigation }: NativeStackScreenProps<RootStackParamList,
         onBack={() => navigation.goBack()}
         title="About"
       />
-      <ScrollView style={{
+      <View style={{
         flex: 1,
         padding: 16,
+        justifyContent: 'space-between',
       }}>
-      </ScrollView>
+        <View>
+          <Text variant='headlineLarge' style={{ fontWeight: '700', marginBottom: 8 }}>
+            MedList
+          </Text>
+          <Text variant='titleLarge' style={{ fontWeight: '500' }}>
+            Built by Abdul Rehman
+          </Text>
+          <Text variant='bodyMedium' style={{ marginTop: 8 }}>
+            MedList is a simple app to help you keep track of your medications.
+          </Text>
+          <Text variant='bodyMedium' style={{ marginTop: 8 }}>
+            I recently came across a problem that people with a lot of medications need to either type down or write down their list of medicines and/or medical supplies on a weekly or monthly basis. This is a very tedious task and can be easily automated with the help of a simple app. Especially given that most medicines remain the same for a long time and only a few medicines are added or removed from the list.
+          </Text>
+          <Text variant='bodyMedium' style={{ marginTop: 8 }}>
+            This app can help you add your medicines and medical supplies to a list and then you can easily share that list with your doctor or pharmacist. You can select which medicines you want to share and which ones you don't want to share.
+          </Text>
+          <Text variant='bodyMedium' style={{ marginTop: 8 }}>
+            I hope this app helps you in keeping track of your medications and medical supplies.
+          </Text>
+        </View>
+        <View>
+          <Text variant='bodyMedium'>
+            Check me out at: <Text variant='bodyMedium' style={{ fontWeight: '700' }} onPress={() => {
+              Linking.openURL('https://abdulrehman.dev');
+            }}>https://abdulrehman.dev</Text>
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
